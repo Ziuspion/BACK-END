e5cdd9262998ba80827eac1532c71d165cb0570e
Orlando Júnior Brito Da Silva <orlandojuniorbritodasilva@gmail.com>
2026-09-18 09:52:51 -0300

    2

---
+++ Aula02/conceito_tipos_de_dados.js
@@ -0,0 +1,12 @@
+// TIpos de dados
+// No JavaScript, tudo o que armazenamos em variáveis tem um tipo. Esses tipos definem o que podemos fazer com os valores.
+
+// Principais tipos de dados:
+// String (texto)
+// Number (número)
+// Boolean (verdadeiro ou falso)
+// Object (Objetos, que agrupam informações)
+// Array (Lista de valores)
+// Null (Valor vazio)
+// undefined (quando algo não foi definido)
+
--- Aula02/index.js
+++ Aula02/index.js
@@ -10,4 +10,28 @@ const pi = 3.14;
 //pi = 3.15; Erro
 console.log(pi);
 
+//----------------------------------
 // Formas de escrever um código
+//----------------------------------
+
+//_________________________________________________________________________________________________
+// Camel case ----- * A mais famosa
+// - Primeira para minúscula
+// - palavras seguites começam com maiúscula
+
+// let nomeCompleto; let idadeUsuario; let functionCalcularIdade (){}
+//_________________________________________________________________________________________________
+// Pascalcase
+// - todas as palavras começam com letra maiúscula
+
+// class UsuarioSistema {
+//     constructor(nome, idade) {
+//         this.nome = nome;
+//         this.idade = idade;
+//     }
+// };
+//_________________________________________________________________________________________________
+// Snake Case
+// - Palavras separadas por underscore (_)
+
+// let nome_completo; let total_vendas;
